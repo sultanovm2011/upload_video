1c384f111879da5c90a004d5fd85e817ac46d65b
sultanovm2011 <48181616+sultanovm2011@users.noreply.github.com>
2019-08-14 00:16:35 +0000

    Update upload_video.py

--- upload_video.py
+++ upload_video.py
@@ -11,67 +11,102 @@ wait = WebDriverWait(driver, 1000)
 driver.get("https://www.spankwire.com/");
 pyautogui.press('f11')
 
-
+def click_wait(elem):
+    i = 0
+    while i<20:        
+        try:
+            elem.click() 
+        except Exception:
+            time.sleep(0.5)
+        else: i += 21
+        finally: i += 1 
+            
 def authorisation():
-    driver.find_element_by_partial_link_text("Upload").click()
-    driver.find_element_by_name("username").send_keys("granula2000")
-    driver.find_element_by_name("password").send_keys("granula2000")
-    driver.find_element_by_xpath("//a[@href='/forgot']/preceding-sibling::button").click()
-    
+    wait.until(EC.presence_of_element_located((By.LINK_TEXT, "Upload")))
+    click_wait(driver.find_element(By.LINK_TEXT, "Upload"))
+    wait.until(EC.element_to_be_clickable((By.NAME,"username"))).send_keys("granula2000")
+    wait.until(EC.element_to_be_clickable((By.NAME,"password"))).send_keys("granula2000")
+    wait.until(EC.element_to_be_clickable((By.XPATH,"//a[@href='/forgot']/preceding-sibling::button"))).click()
+  
+            
 def fileUploading():
-    time.sleep(3)
-#    wait.until(driver.invisibility_of_element_located(By.link_text, "Upload")).click()
-    driver.find_element_by_partial_link_text("Upload").click()
+    i = 0
+    while i<20:        
+        try:
+            driver.find_element(By.LINK_TEXT, "Upload").click()
+        except Exception:
+            time.sleep(0.5)
+        else: i += 21
+        finally: i += 1
+
     # Click to "Choose file to upload" button
-    driver.find_element_by_xpath('//*[@id="js-react-upload-video-form"]//*[contains(text(),"Files from your computer")]/following-sibling::label').click()
-    # Path to file
-    pyautogui.typewrite('/home/max/sk.mp4')
-    pyautogui.press('enter') 
+    i = 0
+    while i<20:        
+        try:
+            driver.find_element_by_xpath('//*[@id="js-react-upload-video-form"]//*[contains(text(),"Files from your computer")]/following-sibling::label').click()
+        except Exception:
+            time.sleep(0.5)
+        else: i += 21
+        finally: i += 1    
+    time.sleep(1)
     
-    # File description
-    elems = driver.find_elements_by_xpath("//div[@class='css-1wy0on6']")
+    # Path to file
+    pyautogui.typewrite('/home/max/sk.mp4', interval=0.03)
+    pyautogui.press('enter') 
+    time.sleep(1)
+
+    # FILE DESCRIPTION
+    
+    elems = driver.find_elements_by_xpath("//div[@class='css-1wy0on6']")    
     # Select "Product type" 
-    elems[0].click()
+    click_wait(elems[0])
     # uncomment nothing to choose "Proffesional"    
 #    pyautogui.press('down') #uncomment to choose "Homemade" 
     pyautogui.press('enter')     
-    time.sleep(1)
 
     # Select "Orientation"
-    elems[1].click()  
+    click_wait(elems[1])
     # uncomment nothing to choose "Straight"
 #    pyautogui.press('down') #uncomment to choose "Tranny" 
 #    for i in range(2): pyautogui.press('down') #uncomment to choose "Gay"
     pyautogui.press('enter') 
-    time.sleep(1)
     elems = driver.find_elements_by_xpath("//div[@class='css-1wy0on6']")
-    time.sleep(1)
 
     # choose "Categories"
-    elems[2].click() 
-#    wait.until(EC.element_to_be_clickable((elems[1]))).click()
-    pyautogui.typewrite('saggy')
-    time.sleep(2)
-    pyautogui.press('enter') 
-    time.sleep(1)
-
-    # Type "title"
-    driver.find_element_by_name("title").send_keys("masturbation")
-    time.sleep(1)
-    
-    # choose "Pornstars"
-    elems[3].click() 
-    pyautogui.typewrite('a.j.')
+    click_wait(elems[2])
+    pyautogui.typewrite('saggy', interval=0.05)
     time.sleep(3)
     pyautogui.press('enter') 
-    time.sleep(1)
+
+    # Type "title"
+    wait.until(EC.element_to_be_clickable((By.NAME,"title"))).send_keys("masturbation")
+
+    # choose "Pornstars"
+    click_wait(elems[3])
+    pyautogui.typewrite('a.j.', interval=0.05)
+    time.sleep(4)
+    pyautogui.press('enter') 
     
     # Type "description"
-    driver.find_element_by_xpath("//textarea[@class='sc-dUjcNx sc-gHboQg sc-elNKlv dTqHfs']").send_keys("orgazm")
-   
+    i = 0
+    while i<20:        
+        try:
+            driver.find_element(By.XPATH,'//*[@id="js-react-upload-video-form"]//*[@name="description"]').send_keys("orgazm")
+        except Exception:
+            time.sleep(0.5)
+        else: i += 21
+        finally: i += 1
+ 
     # Submit
-    wait.until(EC.element_to_be_clickable((By.XPATH, "//button[@class='sc-fYiAbW sc-fOKMvo sc-hUMlYv kFEgXl']"))).click()
+    i = 0
+    while i<20:        
+        try:
+            driver.find_element(By.XPATH, '//*[@id="js-react-upload-video-form"]//*[contains(text(),"Submit")]').click()
+        except Exception:                                
+            time.sleep(0.5)
+        else: i += 21
+        finally: i += 1    
 
-# main
+# MAIN
 authorisation()
 fileUploading()
